c87c9d56ebfe0f3a8bcae9120d0945bce4bf1f42
Olgerd Ševiakov <olgerd@zenitech.co.uk>
2018-09-08 18:35:16 +0300

    optimized imports

--- src/main/java/com/dyadic/SerialMonitorLogger.java
+++ src/main/java/com/dyadic/SerialMonitorLogger.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
